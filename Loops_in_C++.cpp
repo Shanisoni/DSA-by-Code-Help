8f677862b9b3d4caba8787912448bbd7757fd97b
Shani Soni <shanisoni7084@gmail.com>
2024-03-06 19:19:47 +0530

    Topics

---
+++ Loops_in_C++.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+     for(int i = 1;  i<=10;  i=i+1) {
+
+        cout<< "Shani Soni"<< endl ;
+
+    }
+
+     cout<< "Raja RAM "<< endl ;
+
+
+  
+
+   
+}
