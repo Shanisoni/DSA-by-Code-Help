347b662dab22f00739999236491e09e476baad3e
Shani Soni <shanisoni7084@gmail.com>
2024-03-06 21:18:42 +0530

    Pattern

--- Hollow_Pattern.cpp
+++ Hollow_Pattern.cpp
@@ -3,22 +3,18 @@ using namespace std;
 
 int main(){
 
-    int n;
-    cin>> n;
-
-    for(int i = 0;  i <n; i++){
-        for(int j = 0; j <n; j++){
-            if(i==0 || i == n-1 ||j == 0 || j == n-1 ){
-                
-                cout<<"*";               
+    int n ;
+    cin>> n ;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(i==0 || i==n-1 || j==0 || j==n-1){
+                cout<< "*";
             }
             else{
                 cout<<" ";
             }
-
-            
-            
+           
         }
-        cout<<endl;
+         cout<<endl;
     }
 }
---
+++ Pyramid_Pattern.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int N ;
+    cin>> N ;
+    int k = 0;
+   
+    for(int i=0; i<N; i++){
+        
+        for(int j=0; j< ((2*N)-1); j++){
+            if(j<N-i-1){
+                cout<< " ";
+            }
+             else if( k < 2 * i + 1 ){
+                cout<<  "*";
+                k++;
+            }else{
+                cout<< " ";
+            }
+        }
+         cout<<endl;
+    }
+}
