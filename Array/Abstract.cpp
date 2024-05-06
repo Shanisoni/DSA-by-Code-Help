fd32e9664c2b8520d7a1f2cf21025a3aa6e44f58
Shani Soni <shanisoni7084@gmail.com>
2024-05-06 16:42:34 +0530

    Commited

--- Array/Abstract.cpp
+++ Array/Abstract.cpp
@@ -183,6 +183,13 @@ int main() {
   // cout << arr[3] << endl;
   // cout << arr[4] << endl;
 
+  // cout << arr[0] << endl;
+  // cout << arr[1] << endl;
+  // cout << arr[2] << endl;
+  // cout << arr[3] << endl;
+  // cout << arr[4] << endl;
+
+
   //taking input in an array
   // int arr[5] ;
   // int n = 5;
