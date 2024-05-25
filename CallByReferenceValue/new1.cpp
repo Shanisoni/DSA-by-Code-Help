4a491cbaa39c7ef6b37157c601601adec54ee3fe
Shani Soni <shanisoni7084@gmail.com>
2024-05-25 14:26:39 +0530

    Commited

---
+++ CallByReferenceValue/new1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std; 
+
+int main() {
+    // int a = 7;
+
+    // int & c = a;
+
+    // cout<< c<< endl;
+
+    // c++;
+    // cout<< c<< endl;
+
+
+    int a ;
+
+    cin>>  a ;
+
+    a = incrementBy1(a); 
+
+    cout << a << endl;
+
+}
