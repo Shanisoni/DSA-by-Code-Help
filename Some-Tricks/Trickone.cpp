e3476bf8287817ac31004166fcfda04e5be4215e
Shani Soni <shanisoni7084@gmail.com>
2024-06-10 20:30:37 +0530

    Commited

---
+++ Some-Tricks/Trickone.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+
+using namespace std; 
+// We use vector for randm memry allocation inteead  of using it we can use 
+
+
+// or do this by this method 
+int main() {
+    int n; 
+    cin >> n;
+    int*arr = new int[n];
+    
+    
+}
+
