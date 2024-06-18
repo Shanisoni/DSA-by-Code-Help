8baf1ef05d2ed0734c3829e0e3eb2f33a123a4d2
Shani Soni <shanisoni7084@gmail.com>
2024-06-18 08:33:20 +0530

    Commited

--- SumOfN/SumOfN.cpp
+++ SumOfN/SumOfN.cpp
@@ -18,3 +18,25 @@ void sum (int n){
 int main(){
        sum(5);
 }
+
+
+// #include<iostream>
+
+// using namespace std;
+
+
+// void sum (int n){
+//     int result = 0 ;
+//     for(int i = 0 ; i<=n; i++){
+
+//         result = result + i;
+        
+//     }
+//     cout<<"Here is the result"<<endl<<result<<endl;
+
+
+// }
+
+// int main(){
+//        sum(5);
+// }
