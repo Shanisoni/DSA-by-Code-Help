9e0e0375a82fc7d689d9207cedaaf6e84e995cdc
Shani Soni <shanisoni7084@gmail.com>
2024-07-28 13:26:32 +0530

    commited

---
+++ Array/Array_Level-4/1.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+#include <algorithm>
+
+using namespace std ; 
+
+
+ void ShiftNegative(vector<int>& shani , int n) {
+    int j = 0 ; 
+
+    for ( int index = 0 ; index < n ; index++) {
+        if ( shani[index] < 0 ) {
+            swap(shani[index] , shani[j]);
+            j++ ; 
+        }
+
+
+    }
+
+
+ }
+
+
+
+int main () {
+     
+
+    vector<int> shani = { 1, -2, 3, -4, 5, -6, 7, -8, 9, -10 } ;
+
+    int n = shani.size();
+
+    ShiftNegative( shani , n) ;
+
+    for ( int i = 0 ; i < n ; i++) {
+        cout << shani[i] << " " ; 
+    }
+
+     
+
+
+    
+}
+
+    
