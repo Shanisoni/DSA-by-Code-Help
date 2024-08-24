11b142d56b03708a332e32944c00b4213ee8db25
Shani Soni <shanisoni7084@gmail.com>
2024-08-24 12:30:27 +0530

    c

--- Array/Array_Level-3/1.cpp
+++ Array/Array_Level-3/1.cpp
@@ -4,9 +4,13 @@
 using namespace std ;
 
 void shanii(int shani[2][3], int row , int col) {
+
     for( int i = 0 ; i < row ; i ++ ) {
+
         for( int j = 0 ; j < col ;  j++ )  { 
+
             cout << shani[i][j] << " " ; 
+            
         }
 
         cout << endl ;  
