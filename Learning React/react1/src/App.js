d051bfe728c611a0b4526b326994bf63f9fa72a8
Shani Soni <shanisoni7084@gmail.com>
2024-10-18 08:49:45 +0530

    commited

--- Learning React/react1/src/App.js
+++ Learning React/react1/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 // import Component1 from './Component1';
 import Component1 from './Components/Component1';
+import VsComponent from './Components/VsComponent';
 
 function App() {
   return (
     <div className="App">
       <>
       <Component1 check = {false} />
+      <VsComponent/>
       </>
      
     </div>
---
+++ Learning React/react1/src/Components/VsComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function VsComponent() {
+  return (
+    <>
+    <div>VsComponent</div>
+    <h1 className='bg-success'>Welcome to Bootestarp</h1>
+    </>
+  )
+}
+
+export default VsComponent
