26ea580860c7ba66ea0115bb6d63ceb031a06a55
Shani Soni <shanisoni7084@gmail.com>
2024-10-20 09:56:56 +0530

    commited

--- Learning React/React 2/react02/src/UScomponent.js
+++ Learning React/React 2/react02/src/UScomponent.js
@@ -13,23 +13,74 @@
 
 // export default UScomponent
 
+// import React, { useState } from 'react'
 
+// function UScomponent() {
+//     const myarray = useState("Shani")
+//     let ChangState = () => {
+//         myarray[1] ('Soni')
+//     }
+//   return (
+//     <>
+//     <div>UScomponent</div>
+//     <h1> {myarray[0]} </h1>
+//     <button onClick = {ChangState}  >Click Me</button>
+//     </>
+//   )
+// }
 
+// export default UScomponent
 
-import React, { useState } from 'react'
+// import React, { useState } from 'react'
+
+// function UScomponent() {
+//     const [name , setName] = useState("Shani")
+//     let ChangState = () => {
+//          setName('Soni')
+//     }
+//   return (
+//     <>
+//     <div>UScomponent</div>
+//     <h1> {name} </h1>
+//     <button onClick = {ChangState}  >Click Me</button>
+//     </>
+//   )
+// }
+
+// export default UScomponent
+
+import React, { useState } from "react";
 
 function UScomponent() {
-    const myarray = useState("Shani")
-    let ChangState = () => {
-        myarray[1] ('Soni')
-    }
+  const shani = {
+    name: "shani",
+    class: "B.Tech",
+    Roll: 101,
+    City: "Delhi",
+  };
+
+  const [persion , setName] = useState(shani);
+
+  const ChangState = () => {
+    setName({
+      name : "soni",
+      class : "M.Tech",
+      Roll : 202 ,
+      City : "Mumbai"
+
+    })
+  }
+
   return (
     <>
-    <div>UScomponent</div>
-    <h1> {myarray[0]} </h1>
-    <button onClick = {ChangState}  >Click Me</button>
+    <h1> {persion.City}</h1>
+    <h1> {persion.Roll}</h1>
+    <h1> {persion.class}</h1>
+      <div>UScomponent</div>
+      {/* <h1> {Persion} </h1> */}
+      <button onClick={ChangState}>Click Me</button>
     </>
-  )
+  );
 }
 
-export default UScomponent
+export default UScomponent;
