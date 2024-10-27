2a7c3065d9e2cb3b66a0414850bbb5df791092aa
Shani Soni <shanisoni7084@gmail.com>
2024-10-28 02:26:51 +0530

    commited

---
+++ Learning React/React 2/react02/src/ControlledComponent/ControlFormFunct.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function ControlFormFunct() {
+  return (
+    <>
+
+    <form>
+
+        <label htmlFor='' > Enter Name </label>
+        <input type='text' value={""} />
+
+        <br/>
+
+        <input type='submit' value={'Submit'} />
+        
+    </form>
+    
+    <div>ControlFormFunct</div>
+
+    </>
+  )
+}
+
+export default ControlFormFunct
