0e0363642742df5ef1a476361ec4263b0475f63c
Shani Soni <shanisoni7084@gmail.com>
2024-10-30 15:45:21 +0530

    Commited

--- Learning React/React 4/React04/react_rout_demo/src/App.js
+++ Learning React/React 4/React04/react_rout_demo/src/App.js
@@ -1,15 +1,31 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
+import {Routes , Route }  from 'react-router-dom';
+import Home from './Component/Home';
+import About from './Component/About';
+import Contact from './Component/Contact';
+
+
+
 
 function App() {
   return (
     <div>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/Contact' element={<Contact/>} />
+
+
+      </Routes>
 
 
      
     </div>
-  );
+  );  
 }
+
+
 
 export default App;
