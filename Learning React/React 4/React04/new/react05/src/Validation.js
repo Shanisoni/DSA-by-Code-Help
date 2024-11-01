c37b4d4d9a2e76bab0c4e08f6cb20f867f21d13e
Shani Soni <shanisoni7084@gmail.com>
2024-11-01 18:34:33 +0530

    Commited

--- Learning React/React 4/React04/new/react05/src/Validation.js
+++ Learning React/React 4/React04/new/react05/src/Validation.js
@@ -5,10 +5,13 @@ function Validation() {
     const  [name , setName ] = useState ( '');
     const [email , setEmail]  = useState('');
     const [nameError , setNameError] = useState( false  ) ;
+    const [emailError , setEmailError] = useState( false);
+
+    const EmailRegex = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/;
 
     const handleName=  (e) => {
       let name = e.target.value;
-      setName(name);
+     
       if( name.length < 3)
         {
         setNameError( true );
@@ -17,7 +20,54 @@ function Validation() {
         setNameError( false) ;
 
       }
+
+      setName(name);
          
+    }
+
+
+    const handleEmail = (e) => {
+      let email = e.target.value;
+    
+
+
+      if( ! email.match(EmailRegex)) {
+        setEmailError( true);
+      }
+      else {
+        setEmailError( false);
+      }
+      setEmail(email);
+
+    }
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      alert(e.target[0].value + ' ' + e.target[1].value);
+      let name = e.target.value;
+     
+      if( name.length < 3)
+        {
+        setNameError( true );
+      }
+      else{
+        setNameError( false) ;
+
+      }
+
+
+
+      let email = e.target.value;
+    
+
+
+      if( ! email.match(EmailRegex)) {
+        setEmailError( true);
+      }
+      else {
+        setEmailError( false);
+      }
+      setEmail(email);
+
     }
    return (
     <div>
@@ -25,18 +75,19 @@ function Validation() {
 
         <h1> Form Validation </h1>
  
-        <form>
+        <form onSubmit={handleSubmit}>
         <label htmlFor='Name'>  Name </label>
         <input type='text'  name='name' value={name} required  onChange={handleName} />
 
         <br/>
-        {nameError  ? <span> Name must be of  </span> : <span> Name is invalid </span> }
+        {nameError  ? <span style={ {color: 'red'} }> Name must be of 3 words </span> : ' ' }
         <br/>
 
         <label htmlFor='Email'>  Email </label>
-        <input type='text'  name='email'  value={email} required/>
+        <input type='text'  name='email'  value={email} onChange={handleEmail} required/>
 
         <br/>
+        {emailError  ? <span style={ {color: 'red'} }> Incorrect Email </span> : ' ' }
         <br/>
 
         <label htmlFor='Submit'>  Submit </label>
