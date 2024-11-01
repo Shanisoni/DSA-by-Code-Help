4b056db064742bf1d7f66b87a340b7427ed48924
Shani Soni <shanisoni7084@gmail.com>
2024-11-02 00:51:10 +0530

    c

--- Learning React/React 4/React04/new/react05/src/Form.js
+++ Learning React/React 4/React04/new/react05/src/Form.js
@@ -1,6 +1,19 @@
 import React from 'react'
+import { useFormik } from 'formik';
 
 function Form() {
+
+    const formInitialValues = {
+        name : '',
+        email : ''
+    }
+
+  const formik = useFormik({
+     initialValues : formInitialValues
+
+  });
+
+
   return (
     <div style={{position : 'center'}}>
 
@@ -11,18 +24,18 @@ function Form() {
 
         <form style={{position : 'center'}}>
             <label htmlFor='' > Enter Your Name </label>
-            <input type='text' name='name' />
+            <input type='text' name='name' value={formik.values.name} onChange={formik.handleChange} />
             <br/>
             <br/>
 
             <label htmlFor='' > Enter Your Email </label>
-            <input type='email' name='name' />
+            <input type='email' name='name' value={formik.values.email} onChange={formik.handleChange} />
 
             <br/>
             <br/>
 
-            <label htmlFor='' > Submit </label>
-            <input type='sumit' name='name' value= 'Submit' />
+            {/* <label htmlFor='' > Submit </label> */}
+            <input type='submit' name='name' value= 'Submit' />
 
         </form>
         
