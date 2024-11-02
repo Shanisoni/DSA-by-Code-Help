c713896f1ed1735f623a6e7ff25511fc616ac730
Shani Soni <shanisoni7084@gmail.com>
2024-11-02 21:33:56 +0530

    c

--- Learning React/React 4/React04/new/react05/src/FormikForm2.js
+++ Learning React/React 4/React04/new/react05/src/FormikForm2.js
@@ -4,13 +4,26 @@ import React from 'react'
 function FormikForm2() {
   return (
     <div>
-        <h1>Welcome To Formik 2 </h1>
-        <Formik>
+        <h1>Welcome To Formik Form  </h1>
+        <Formik initialValues={{name : 'Shani'}} 
+        onSubmit={(values) => {
+
+            console.log(values);
+            
+
+        }}
+        
+        
+        >
 
             <Form>
-                
+
             <label htmlFor='' > Enter Your Name </label>
             <Field type='text' name='name' />
+            <br/>
+            <br/>
+
+            <button type='submit' > Submit </button>
 
             </Form>
             
