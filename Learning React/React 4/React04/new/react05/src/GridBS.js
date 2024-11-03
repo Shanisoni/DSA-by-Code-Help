1d5e4ed40e0b6801aedba7df8d22ad42cabb5f80
Shani Soni <shanisoni7084@gmail.com>
2024-11-03 12:40:11 +0530

    C

---
+++ Learning React/React 4/React04/new/react05/src/GridBS.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Alert, Button , Row, Col } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function GridBS() {
+  return (
+    <div>GridBS</div>
+  )
+}
+
+export default GridBS
