119e5d3e25694c26b0776e0c8383d6b96daef107
Shani Soni <shanisoni7084@gmail.com>
2024-11-04 10:13:55 +0530

    Commited

---
+++ Learning React/React 4/React04/new/react05/src/Carousel.js
@@ -0,0 +1,37 @@
+import { Component , Row , Col } from 'react-bootstrap'
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function Carousel() {
+  return (
+    <div>
+        <>
+        <Component>
+            <Row>
+                <Col>
+                <Carousel>
+                    <Carousel.Item>
+                        
+                    </Carousel.Item>
+
+                </Carousel>
+
+                </Col>
+
+            </Row>
+
+        </Component>
+
+
+        </>
+        
+        
+        
+        
+        
+        
+    </div>
+  )
+}
+
+export default Carousel
