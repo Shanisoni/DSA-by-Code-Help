0ed2ed9399f0a6be5a0ae8efc59abb750994fe02
Shani Soni <shanisoni7084@gmail.com>
2024-11-05 04:43:11 +0530

    Commited

--- Learning React/React 4/React04/new/react05/src/API.js
+++ Learning React/React 4/React04/new/react05/src/API.js
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
 
 function Api() {
 
-    fetch('https://jsonplaceholder.typicode.com/posts')
-    .them(  (response) => {
+    const [posts , setPosts] = useState([]);
+
+    useEffect( () => {
+      fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(  (response) => {
+
+      response.json().then( (result) =>{
+        setPosts(result);
+        console.log(result);
+      })
         
         console.log(response);
+    })
     })
   return (
     <div>
 
         <h1>
             Fetch API
+            <ul>
+              {
+                posts.map( (post) => <li key={post.id}>{post.title}</li>) 
+              }
+            </ul>
         </h1>
     
     
