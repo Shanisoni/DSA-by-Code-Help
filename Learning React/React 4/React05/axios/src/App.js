d6eb23d895bdeeba2bc54eb19843ba7da70a8a1e
Shani Soni <shanisoni7084@gmail.com>
2024-11-05 05:17:50 +0530

    C

--- Learning React/React 4/React05/axios/src/App.js
+++ Learning React/React 4/React05/axios/src/App.js
@@ -1,9 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
+import GetAxios from './GetAxios';
 
 function App() {
   return (
     <>
+    <GetAxios/>
 
     </>
     
---
+++ Learning React/React 4/React05/axios/src/GetAxios.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function GetAxios() {
+    const [data, setData] = useState([]);
+
+    const baseUrl = 'https://jsonplaceholder.typicode.com/posts';
+
+    useEffect(() =>  {
+        axios.get(baseUrl).then((response) => {
+            setData(response.data); 
+        });
+    }, []); // Dependency array to run this effect only once
+
+    return (
+        <>
+            <h1>Posts</h1>
+            <ul>
+                {data.map((post) => (
+                    <li key={post.id}>
+                        <h2>{post.title}</h2>
+                        <p>{post.body}</p>
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
+}
+
+export default GetAxios;
