67d026689801db3a133c49a160c4bdc4605a5452
Shani Soni <shanisoni7084@gmail.com>
2024-11-05 16:17:28 +0530

    C

--- Learning React/React 4/React05/React06/crud-app/src/Component/Read.js
+++ Learning React/React 4/React05/React06/crud-app/src/Component/Read.js
@@ -1,14 +1,76 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 
 function Read() {
+
+  function getData() {
+    axios.get('https://6729ac1e6d5fa4901b6de2b6.mockapi.io/crud')
+    .then( (response) => {
+      console.log(response.data)
+    })
+  }
   return (
-    <div>
+    <>
+    <div className='row'>
+      <div className='col-md-12'>
+        <table className='table table-bordered table-striped table-dark table-hover' >
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Age</th>
+              <th>Email</th>
+              <th>Edit</th>
+              <th>DELETE</th>
+
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>John Doe</td>
+              <td>30</td>
+              <td>shanisoni7083@gmail.com</td>
+              <td>Edit </td>
+              <td>Delete</td>
+            </tr>
+
+            <tr>
+              <td>1</td>
+              <td>John Doe</td>
+              <td>30</td>
+              <td>shanisoni7083@gmail.com</td>
+              <td>Edit </td>
+              <td>Delete</td>
+            </tr>
+
+            <tr>
+              <td>1</td>
+              <td>John Doe</td>
+              <td>30</td>
+              <td>shanisoni7083@gmail.com</td>
+              <td>Edit </td>
+              <td>Delete</td>
+            </tr>
+
+            <tr>
+              <td>1</td>
+              <td>John Doe</td>
+              <td>30</td>
+              <td>shanisoni7083@gmail.com</td>
+              <td>Edit </td>
+              <td>Delete</td>
+            </tr>
+
+          </tbody>
+        </table>
+      </div>
         
-        <h1>
-            Read
-        </h1>
+     
         
     </div>
+    </>
   )
 }
 
