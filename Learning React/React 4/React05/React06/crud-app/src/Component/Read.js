b11f40919e8911e3e1e4dc33df766e28de463e7f
Shani Soni <shanisoni7084@gmail.com>
2024-11-05 16:46:06 +0530

    C

--- Learning React/React 4/React05/React06/crud-app/src/Component/Read.js
+++ Learning React/React 4/React05/React06/crud-app/src/Component/Read.js
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
+import { useState } from 'react';
 
 function Read() {
+
+  const [APIdata, setAPIdata] = useState([]);
+
 
   function getData() {
     axios.get('https://6729ac1e6d5fa4901b6de2b6.mockapi.io/crud')
     .then( (response) => {
-      console.log(response.data)
+      setAPIdata(response.data)
     })
   }
+
+  useEffect( () => {
+    getData();
+  } , [])
 
   getData();
   return (
@@ -29,42 +37,33 @@ function Read() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>1</td>
-              <td>John Doe</td>
-              <td>30</td>
-              <td>shanisoni7083@gmail.com</td>
-              <td>Edit </td>
-              <td>Delete</td>
-            </tr>
+              { 
+                APIdata.map( (data) => {
+                  return (
+                    <tr key={data.id}>
+                      <td>{data.id}</td>
+                      <td>{data.Employee_name}</td>
+                      <td>{data.Employee_Age}</td>
+                      <td>{data.Employee_Email}</td>
+                      <td><button className='btn btn-primary'>Edit</button></td>
+                      <td><button className='btn btn-danger'>Delete</button></td>
+                    </tr>
+                  )
+                })
 
-            <tr>
-              <td>1</td>
-              <td>John Doe</td>
-              <td>30</td>
-              <td>shanisoni7083@gmail.com</td>
-              <td>Edit </td>
-              <td>Delete</td>
-            </tr>
 
-            <tr>
-              <td>1</td>
-              <td>John Doe</td>
-              <td>30</td>
-              <td>shanisoni7083@gmail.com</td>
-              <td>Edit </td>
-              <td>Delete</td>
-            </tr>
 
-            <tr>
-              <td>1</td>
-              <td>John Doe</td>
-              <td>30</td>
-              <td>shanisoni7083@gmail.com</td>
-              <td>Edit </td>
-              <td>Delete</td>
-            </tr>
 
+                // <tr>
+                //         <td>{item.id} </td>
+                //         <td>{item.Employee_name} </td>
+                //         <td>{item.Employee_Age} </td>
+                //         <td>{item.Employee_Email} </td>
+                //         <td>Edit </td>
+                //         <td>Delete</td>
+                //       </tr>
+                 
+              }
           </tbody>
         </table>
       </div>
