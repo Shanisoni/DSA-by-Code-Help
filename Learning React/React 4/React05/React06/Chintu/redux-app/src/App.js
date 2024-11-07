dd9fa873a3eb93835036b5f6570b3b8ddc41c1b6
Shani Soni <shanisoni7084@gmail.com>
2024-11-07 09:41:18 +0530

    Commited

--- Learning React/React 4/React05/React06/Chintu/redux-app/src/App.js
+++ Learning React/React 4/React05/React06/Chintu/redux-app/src/App.js
@@ -4,6 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>
+        Welcome to Redux App
+      </h1>
+      <input type="text" placeholder="Enter your data " />
+      <br/>
+      <br/>
+      <button type="button"> Increment </button>
+      <br/>
+      <br/>
+      <button type="button"> Decrement </button>
       
     </div>
   );
