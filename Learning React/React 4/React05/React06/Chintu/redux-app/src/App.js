5a11bf789f62a19b84fada5f9ebbc6a4f18369ca
Shani Soni <shanisoni7084@gmail.com>
2024-11-08 10:31:29 +0530

    commited

--- Learning React/React 4/React05/React06/Chintu/redux-app/src/App.js
+++ Learning React/React 4/React05/React06/Chintu/redux-app/src/App.js
@@ -4,13 +4,13 @@ import { useSelector  } from 'react-redux';
 
 function App() {
 
-  const myState = useSelector ( (State) => State.Inc_Dec);
+  const myState = useSelector ( (state) => State.Inc_Dec);
   return (
     <div className="App">
       <h1 mt-4>
         Welcome to Redux App
       </h1>
-      <input type="text" placeholder="Enter your data "  value={myState} />
+      <input type="text"   value={myState} />
       <br/>
       <br/>
       <button type="button"  className = 'btn btn-primary'> Increment </button>
